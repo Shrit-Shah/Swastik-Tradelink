6df27dbb36f89dcb71abb45beb3d00984cd2d8d2
priyanshiakbari <68318673+priyanshiakbari@users.noreply.github.com>
2021-02-25 23:05:48 +0530

    adjusted the homepage

--- tradelink/urls.py
+++ tradelink/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-import store
+from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='Homepage')
+
 ]
